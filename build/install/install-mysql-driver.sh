e7426541cc00f9dbd8fed5ace7812daf9044d6d9
James Blanksby <james@thisismyengine.com>
2017-01-13 11:15:33 +0000

    Updated driver

--- build/install/install-mysql-driver.sh
+++ build/install/install-mysql-driver.sh
@@ -1,28 +1,28 @@
 cd /tmp
 
 # download MySQL 5 driver
-wget -O mysql-connector-java-5.1.34.tar.gz https://www.dropbox.com/s/oe18zd9hqehu0a7/mysql-connector-java-5.1.34.tar?dl=1
+wget -O mysql-connector-java-5.1.40.tar.gz https://www.dropbox.com/s/6txfasgc9xfjd6x/mysql-connector-java-5.1.40.tar.gz?dl=1
 
 # extract jar file
-tar -zxvf mysql-connector-java-5.1.34.tar.gz
+tar -zxvf mysql-connector-java-5.1.40.tar.gz
 
 # copy jar file to cf directory
-cd mysql-connector-java-5.1.34
-cp mysql-connector-java-5.1.34-bin.jar /opt/coldfusion2016/cfusion/lib
+cd mysql-connector-java-5.1.40
+cp mysql-connector-java-5.1.40-bin.jar /opt/coldfusion2016/cfusion/lib
 
 # fix permissions
-chown coldfusion /opt/coldfusion2016/cfusion/lib/mysql-connector-java-5.1.34-bin.jar
-chmod 0700 /opt/coldfusion2016/cfusion/lib/mysql-connector-java-5.1.34-bin.jar
+chown coldfusion /opt/coldfusion2016/cfusion/lib/mysql-connector-java-5.1.40-bin.jar
+chmod 0700 /opt/coldfusion2016/cfusion/lib/mysql-connector-java-5.1.40-bin.jar
 
 # restart cf
 /opt/coldfusion2016/cfusion/bin/coldfusion restart
 
-if [ -f "mysql-connector-java-5.1.34.tar.gz" ]
+if [ -f "mysql-connector-java-5.1.40.tar.gz" ]
 then
-    rm mysql-connector-java-5.1.34.tar.gz
+    rm mysql-connector-java-5.1.40.tar.gz
 fi
 
-if [ -d "mysql-connector-java-5.1.34" ]
+if [ -d "mysql-connector-java-5.1.40" ]
 then
-    rm -rf mysql-connector-java-5.1.34
+    rm -rf mysql-connector-java-5.1.40
 fi
